ec572eb61debbd735896146d1b6994a133985eb1
Lucas Samuel <lukssds@gmail.com>
2022-02-01 23:28:07 -0300

    Prjeto joslin youtube-api

---
+++ src/main/java/com/joslin/youtubedataapi/YoutubeDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.joslin.youtubedataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeDataApiApplication.class, args);
+	}
+
+}
